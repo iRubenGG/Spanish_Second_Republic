91a165ba8ebc5fb6df2b64f4ce176cc3f255d61c
iRubenGG <ruben.gallardo.cat@gmail.com>
2026-02-20 00:47:25 +0000

    name

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "No Pasaran! A Seoncd Spanish Republic Experience" + '_' + "iRubenGG";
+  var TITLE = "No Pasaran! A Second Spanish Republic Experience" + '_' + "iRubenGG";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
